19dd2bec5363b42fa3c9c666a9a573c1112de76e
belli49 <pedrokomessu@gmail.com>
2022-05-21 19:22:32 +0900

    Connected NodeJS server to localily hosted MySQL database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
+var mysql = require('mysql');
 
 // Require dotenv file
 require('dotenv').config();
@@ -9,7 +10,40 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Connected to Discord!');
+});
+
+// Connect to MySQL server
+var sql_con = mysql.createConnection({
+  host: "localhost",
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD
+});
+
+// Check if database connection succeeded
+sql_con.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected to Database!");
+})
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(
+            `Server name: ${interaction.guild.name}\n
+            Total members: ${interaction.guild.memberCount}`
+        );
+	} else if (commandName === 'user') {
+		await interaction.reply(
+            `Your tag: ${interaction.user.tag}\n
+            Your id: ${interaction.user.id}`
+        );
+	}
 });
 
 client.on('message', msg => {
